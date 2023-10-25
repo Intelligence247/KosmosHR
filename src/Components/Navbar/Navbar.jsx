82d5a3216a39b3bb000d89a277d73eb490885851
Intelligence247 <ua67527056@gmail.com>
2023-10-25 01:15:22 +0100

    Completed the mobile view of Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
+  const navCenterArr = ["Training", "Chat Bot"];
+  const [switchs, setswitchs] = useState(0);
+  console.log(switchs);
   return (
-    <div>
-    </div>
-  )
-}
+    <div className="navWrapper px-20 py-6">
+      <div className="leftNav">
+        <img src="/Logo2.png" alt="" />
+        <p>SecureBot</p>
+      </div>
 
-export default Navbar
+      <div className="centerNav">
+        {navCenterArr.map((n, i) => (
+          <p
+            key={i}
+            onClick={() => setswitchs(i)}
+            className={`${switchs === i ? "bg-[#F8E89D]" : "bg-[#FDF9E7]"} cursor-pointer`}
+          >
+            <b className={`${switchs === i ? 'block':'hidden'}`}></b>
+            {n}
+          </p>
+        ))}
+      </div>
+
+      <div className="rightNav">
+        AY
+        <p></p>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
