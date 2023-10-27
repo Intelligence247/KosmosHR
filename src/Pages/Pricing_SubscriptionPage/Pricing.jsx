ad83e200f5b9323d2aa375555ec762740f39d51b
PrimeFord <bashfordbash@gmail.com>
2023-10-27 12:36:41 +0100

    feat: pricing Page

--- src/Pages/Pricing_SubscriptionPage/Pricing.jsx
+++ src/Pages/Pricing_SubscriptionPage/Pricing.jsx
@@ -3,12 +3,12 @@ import Prices from "../../Components/Pricing/Prices";
 
 const Pricing = () => {
   return (
-    <div className="w-[100%] h-[100vh] bg-black bg-opacity-20 flex justify-center items-center">
-      <div className="w-[80%] bg-white rounded-lg text-center p-[2rem]">
+    <div className="w-[100%] bg-black bg-opacity-20 flex justify-center items-center">
+      <div className="w-[80%] my-10 bg-white rounded-lg text-center p-[2rem]">
         <p className="text-[2rem] font-semibold">
           Set up your site, pick a plan
         </p>
-        <p className="text-[0.9rem]">
+        <p className="mt-4 text-[0.85rem]">
           Choose the plan that’s right for your business, is important for
           adequate employment management
         </p>
