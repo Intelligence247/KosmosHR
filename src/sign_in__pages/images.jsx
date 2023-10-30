48aafd22eced2ec09b230240917ebe60cff07521
khennyboy <abdullateefkehinde848@gmail.com>
2023-10-30 17:49:59 +0100

    added

--- src/sign_in__pages/images.jsx
+++ src/sign_in__pages/images.jsx
@@ -2,11 +2,23 @@
 const Images
  = () => {
     
-    useEffect(()=>{
-        const box1 = document.getElementById("box1");
-        const box2 = document.getElementById("box2")
-    })
+    let x = document.querySelector("#box1");
+    let y = document.querySelector("#box2");
+    function animate() {
+        setTimeout(function () {
+            x.style.left = "-900px";
+            y.style.left = "0px";
+            setTimeout(function () {
+                x.style.left = "0px";
+                y.style.left = "900px";
+                setTimeout(function () {
+                    animate(); // Call the animate function recursively
+                }, 4000); // Delay before calling animate again
+            }, 9000); // Delay for the second part of the animation
+        }, 5000); // Delay for the first part of the animation
+    }
 
+    animate();
 
     return ( <div className="image_wrapper">
 
@@ -14,17 +26,21 @@ const Images
 
 <div className="texts">
         <div id="box1">
+            <div className="display">
             <h3>Fostering Management-Staff Harmony!</h3>
             <p>Efforless Task Delegation, Seamless End-of-Day Reports, Improved cross-department communication</p>
         <div></div>
         <div></div>
         </div>
+        </div>
 
         <div id="box2">
+            <div className="display">
             <h3>Task, Report, Connect</h3>
             <p>Revolutionizing Employee Management:</p>
         <div></div>
         <div></div>
+        </div>
         </div>
 
         </div>
--- src/sign_in__pages/signIn.jsx
+++ src/sign_in__pages/signIn.jsx
@@ -2,13 +2,14 @@ import React from "react";
 
 const Sign = () => {
     return ( 
-        <div className="sign">
+        <div className="form">
+         <div className="sign">
            <h2>Sign into Company Account</h2> 
-           <form action="post">
+           <form action="post" noValidate>
            <h3>Email:</h3>
-           <input type="text" name="email" id="Email" placeholder="Enter Your Email" />
+           <input type="text" name="email" id="Email" placeholder="Enter Your Email" required />
            <h3>Password:</h3>
-           <input type="password" name="password" id="password" placeholder="Enter Your Password" />
+           <input type="password" name="password" id="password" placeholder="Enter Your Password"  required/>
            <a href="#">Click if ou Lost Your Password</a>
            <button type="submit">Log In</button>
            </form>
@@ -16,6 +17,7 @@ const Sign = () => {
            <div className="register">
             Not a member? 
             <a href="#">Create an Account</a>
+           </div>
            </div>
         </div>
      );
